78654d8d36049896781d3cd565d3ed222faa921a
Madhav Appaneni <appanenim@gmail.com>
2022-03-30 21:56:31 +0000

    Sync LeetCode submission - Search a 2D Matrix (python3)

--- my-folder/problems/search_a_2d_matrix/solution.py
+++ my-folder/problems/search_a_2d_matrix/solution.py
@@ -1,35 +1,15 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row_start, row_end, row_num = 0, len(matrix) - 1, 0
-    
-        while row_start <= row_end:
-            row_mid = row_start + (row_end - row_start) // 2
-
-            if matrix[row_mid][0] == target:
-                row_num = row_mid
-                break
-            elif matrix[row_mid][0] > target:
-                row_end = row_mid - 1
-            else:
-                row_start = row_mid + 1
-
-            row_num = row_end
-
-
-        col_start, col_end, col_num = 0, len(matrix[0]) - 1, 0
-
-        while col_start <= col_end:
-            col_mid = col_start + (col_end - col_start) // 2
-            # print(col_mid, col_start, col_end, matrix[row_num][col_mid])
-            if matrix[row_num][col_mid] == target:
-                # return row_num, col_mid
+        row, col = 0, len(matrix[0]) - 1
+        
+        while row >= 0 and col >= 0 and row <= len(matrix)-1 and col <= len(matrix[0]) - 1:
+            # print(row, col)
+            if matrix[row][col] == target:
                 return True
-
-            elif matrix[row_num][col_mid] > target:
-                col_end = col_mid - 1
-
+            elif matrix[row][col] > target:
+                col -= 1
             else:
-                col_start = col_mid + 1
-            # print(col_mid, col_start, col_end)
-        # return -1
+                row += 1
+            
         return False
+                
