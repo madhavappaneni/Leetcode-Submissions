993576e446db9b1030330b8aae006fc98affe243
Madhav Appaneni <appanenim@gmail.com>
2022-05-30 01:28:48 +0000

    Sync LeetCode submission - Valid Perfect Square (python3)

--- my-folder/problems/valid_perfect_square/solution.py
+++ my-folder/problems/valid_perfect_square/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        low, hi = 1, num
+        low, high = 1, num
 
-        while low <= hi:
-            mid = low + (hi - low) // 2
-            # print(low, hi, mid)
+        while low <= high:
+            mid = (low + high) // 2
+            
             if mid ** 2 == num:
-                return mid
+                return True
             
-            if mid ** 2 > num:
-                hi = mid - 1
-            
-            if mid ** 2 < num:
+            elif mid ** 2 > num:
+                high = mid - 1
+            else:
                 low = mid + 1
-        
-        return None
+        return False
