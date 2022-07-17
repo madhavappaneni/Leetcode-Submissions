2ea751ebcdbe9637f60ad69a631ea78be9e4f705
Madhav Appaneni <appanenim@gmail.com>
2022-07-17 14:52:54 +0000

    Sync LeetCode submission - Copy List with Random Pointer (python3)

--- my-folder/problems/copy_list_with_random_pointer/solution.py
+++ my-folder/problems/copy_list_with_random_pointer/solution.py
@@ -9,22 +9,23 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        curr = head
-        head2 = Node(0)
-        curr2 = head2
+        dummy = curr = head
         h = {}
-        while curr:
-            curr2.next = Node(curr.val, curr.next, None)
-            h[curr] = curr2.next
-            curr = curr.next
-            curr2 = curr2.next
-        # print(h)
-        curr2 = head2.next
-        curr = head
-        while curr2:
-            if curr.random:
-                curr2.random = h[curr.random]
-            curr2 = curr2.next
-            curr = curr.next
         
-        return head2.next
+        newDummy = newCurr = Node(0)
+        
+        while curr:
+            newCurr.next = Node(curr.val, curr.next)
+            h[curr] = newCurr.next
+            newCurr = newCurr.next
+            curr = curr.next
+
+        newCurr = newDummy.next
+        curr = head
+        
+        while newCurr and curr:
+            if curr.random:
+                newCurr.random = h[curr.random]
+            newCurr = newCurr.next
+            curr = curr.next
+        return newDummy.next
