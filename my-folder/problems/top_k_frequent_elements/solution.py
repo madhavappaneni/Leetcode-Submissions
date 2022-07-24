f7059d1c2e94f7b29561e3128021e07c9501b908
Madhav Appaneni <appanenim@gmail.com>
2022-07-24 14:10:55 +0000

    Sync LeetCode submission - Top K Frequent Elements (python3)

--- my-folder/problems/top_k_frequent_elements/solution.py
+++ my-folder/problems/top_k_frequent_elements/solution.py
@@ -1,3 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [a[0] for a in Counter(nums).most_common(k)]
+        count = Counter(nums)
+        print(count)
+        sortedCount = sorted(count.items(), key = lambda x : -1 * x[1])
+        print(sortedCount)
+        out = []
+        for i in range(k):
+            out.append(sortedCount[i][0])
+        return out
+        
