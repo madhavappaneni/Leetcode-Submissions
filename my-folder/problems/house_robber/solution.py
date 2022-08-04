152c964414f683efb6219ea3f8cc1cfd0661b793
Madhav Appaneni <appanenim@gmail.com>
2022-08-04 16:24:27 +0000

    Sync LeetCode submission - House Robber (python3)

--- my-folder/problems/house_robber/solution.py
+++ my-folder/problems/house_robber/solution.py
@@ -1,17 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
+        n = len(nums)
+        if n == 1:
             return nums[0]
-        
-        mem = [0] * (len(nums))
-        mem[0] = nums[0]
-        mem[1] = nums[1]
-        
-        for i in range(2, len(nums)):
-            for j in range(i-1):
-                mem_j = mem[j]
-                mem[i] = max(mem[i], nums[i] + mem_j)
-            
-        # print(mem)
-        return max(mem)
-        
+        first, second = nums[0], max(nums[0], nums[1])
+        for i in range(2, n):
+            current = max(first + nums[i], second)
+            first = second
+            second = current
+        return second
