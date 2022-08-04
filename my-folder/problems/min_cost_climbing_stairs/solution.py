6a6223dd9d6f23b8c7548446499f239763dfd01e
Madhav Appaneni <appanenim@gmail.com>
2022-08-04 14:46:32 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (python3)

--- my-folder/problems/min_cost_climbing_stairs/solution.py
+++ my-folder/problems/min_cost_climbing_stairs/solution.py
@@ -3,8 +3,6 @@ class Solution:
         dp = [0] * len(cost)
         dp[0] = cost[0]
         dp[1] = cost[1]
-        resCost = 0
         for i in range(2, len(cost)):
             dp[i] = cost[i] + min(dp[i-1], dp[i-2])
         return min(dp[-1], dp[-2])
-            
