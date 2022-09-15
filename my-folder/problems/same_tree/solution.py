be25ac88b329b34388cf4dd3c46c31465a375476
Madhav Appaneni <appanenim@gmail.com>
2022-09-15 01:25:10 +0000

    Sync LeetCode submission - Same Tree (python3)

--- my-folder/problems/same_tree/solution.py
+++ my-folder/problems/same_tree/solution.py
@@ -1,23 +1,38 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isSameTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
-        if p is None and q is None:
-            return  True
-        if p is None or q is None:
-            return  False
-        if p.val != q.val:
-            return False
-        ans1 = self.isSameTree(p.left, q.left)
-        ans2 = self.isSameTree(p.right, q.right)
-        return ans1 and ans2
- 
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def check(p, q):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return True
+        
+        deq = collections.deque()
+        deq.append((p, q))
+        while deq:
+            p, q = deq.popleft()
+            if not check(p, q):
+                return False
+            
+            if p:
+                deq.append((p.left, q.left))
+                deq.append((p.right, q.right))
+        return True
+            
+    
+# class Solution:
+#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+#         if not p and not q:
+#             return True
+#         if not p or not q:
+#             return False
+#         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            
