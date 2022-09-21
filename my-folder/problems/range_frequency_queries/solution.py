e040d2ced240a0d76d0e60aeef82fb042f829bcf
Madhav Appaneni <appanenim@gmail.com>
2022-09-21 03:38:45 +0000

    Sync LeetCode submission - Range Frequency Queries (python3)

--- my-folder/problems/range_frequency_queries/solution.py
+++ my-folder/problems/range_frequency_queries/solution.py
@@ -1,24 +1,16 @@
 class RangeFreqQuery:
 
     def __init__(self, arr: List[int]):
-        self.arr = arr
-        self.h = {}
+        self.h = collections.defaultdict(list)
         for i, val in enumerate(arr):
-            val = str(val)
-            if val in self.h:
-                self.h[val].append(i) 
-            else:
-                self.h[val] = [i]
-        # print(self.h)
-        
+            self.h[val].append(i)
+
     def query(self, left: int, right: int, value: int) -> int:
-        if str(value) not in self.h:
-            return 0
-        i = bisect.bisect(self.h[str(value)], left - 1)
-        j = bisect.bisect(self.h[str(value)], right)
+        i = bisect.bisect(self.h[value], left - 1)
+        j = bisect.bisect(self.h[value], right)
         return j - i
-    
-    
+
+
 # Your RangeFreqQuery object will be instantiated and called as such:
 # obj = RangeFreqQuery(arr)
 # param_1 = obj.query(left,right,value)
