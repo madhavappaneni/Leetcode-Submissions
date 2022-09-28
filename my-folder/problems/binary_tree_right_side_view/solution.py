04b70068f64954a42f1d53d3b9e33415e64717da
Madhav Appaneni <appanenim@gmail.com>
2022-09-28 00:59:30 +0000

    Sync LeetCode submission - Binary Tree Right Side View (python3)

--- my-folder/problems/binary_tree_right_side_view/solution.py
+++ my-folder/problems/binary_tree_right_side_view/solution.py
@@ -6,17 +6,19 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if root == None:
-            return []
-        
-        rightside = []
-        
-        def helper(node, level):
-            if level == len(rightside):
-                rightside.append(node.val)
-            for child in [node.right, node.left]:
-                if child:
-                    helper(child, level + 1)
-                    
-        helper(root, 0)
-        return rightside
+        if not root:
+            return None
+        deq = collections.deque()
+        deq.append(root)
+        out = []
+        while deq:
+            l = len(deq)
+            localOut = 0
+            for i in range(l):
+                poppedElem = deq.pop()
+                if i == l - 1:
+                    localOut = (poppedElem.val)
+                if poppedElem.left: deq.appendleft(poppedElem.left)
+                if poppedElem.right: deq.appendleft(poppedElem.right)
+            out.append(localOut)
+        return out
