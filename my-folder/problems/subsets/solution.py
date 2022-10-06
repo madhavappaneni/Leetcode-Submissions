759aa6f2d7ccf1539f56a8f88242eef967731491
Madhav Appaneni <appanenim@gmail.com>
2022-10-06 20:42:23 +0000

    Sync LeetCode submission - Subsets (python3)

--- my-folder/problems/subsets/solution.py
+++ my-folder/problems/subsets/solution.py
@@ -1,24 +1,11 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        
-        subset = []
-        
-        def dfs(i):
-            if i >= len(nums):
-                res.append(subset.copy())
-                return
-            subset.append(nums[i])
-            dfs(i+1)
-            
-            subset.pop()
-            dfs(i+1)
-        
-        dfs(0)
-        return res
-        
-        
-        
-            
-                
-                
+        def backtrack(first, curr):
+            out.append(curr[:])
+            for idx in range(first, len(nums)):
+                curr.append(nums[idx])
+                backtrack(idx + 1, curr)
+                curr.pop()
+        out = []
+        backtrack(0, [])
+        return out
