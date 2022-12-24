21b628ac18e5b932634d7a4eb0fb20100a3898bc
Madhav Appaneni <appanenim@gmail.com>
2022-12-24 01:45:36 +0000

    Sync LeetCode submission - Subsets II (python3)

--- my-folder/problems/subsets_ii/solution.py
+++ my-folder/problems/subsets_ii/solution.py
@@ -1,23 +1,14 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         out = []
-        subset = []
-        n = len(nums)
         nums.sort()
-        def dfs(first=0):
-            if first == k:
-                out.append(subset[:])
-                return
-            if first > k:
-                return
-            
-            for i in range(first, n):
-                if i > first and nums[i] == nums[i-1]:
+        def helper(currentIndex, currArr):
+            out.append(currArr.copy())
+            for i in range(currentIndex, len(nums)):
+                if i != currentIndex and nums[i] == nums[i-1]:
                     continue
-                subset.append(nums[i])
-                dfs(i+1)
-                subset.pop()
-                
-        for k in range(n + 1):
-            dfs()
+                currArr.append(nums[i])
+                helper(i + 1, currArr)
+                currArr.pop()
+        helper(0, [])
         return out
