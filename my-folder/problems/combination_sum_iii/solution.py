4d6143b534d2ff3098d48c2b527560caa771f3cb
Madhav Appaneni <appanenim@gmail.com>
2022-12-24 00:08:32 +0000

    Sync LeetCode submission - Combination Sum III (python3)

--- my-folder/problems/combination_sum_iii/solution.py
+++ my-folder/problems/combination_sum_iii/solution.py
@@ -1,23 +1,16 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        output = []
-        
-        def dfs(target, path, idx):
-            if target == 0 and len(path) == k:
-                output.append(list(path))
+        out = []
+        def helper(target, currIndex, k, currArr):
+            if target < 0 or k < 0:
                 return
-            elif target < 0 or len(path) == k:
-                return
-            
-            for i in range(idx, 9):
-                path.append(i+1)
-                dfs(target - i - 1, path, i + 1)
-                path.pop()
-            
-        dfs(n, [], 0)
+            elif target == 0 and k == 0:
+                out.append(currArr.copy())
+            else:
+                for idx in range(currIndex, 10):
+                    currArr.append(idx)
+                    helper(target - idx, idx + 1, k - 1, currArr)
+                    currArr.pop()
+        helper(n, 1, k, [])
+        return out
         
-        return output
-                
-            
-            
-                
