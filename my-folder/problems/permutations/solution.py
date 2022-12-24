b35ddec42b208a19b6128b6b838c6ac9c2ddc027
Madhav Appaneni <appanenim@gmail.com>
2022-12-24 00:31:23 +0000

    Sync LeetCode submission - Permutations (python3)

--- my-folder/problems/permutations/solution.py
+++ my-folder/problems/permutations/solution.py
@@ -2,13 +2,13 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         out = []
         n = len(nums)
-        def dfs(first):
-            if first == n:
-                out.append(nums[:])
-            
-            for i in range(first, n):
-                nums[first], nums[i] = nums[i], nums[first]
-                dfs(first + 1)
-                nums[first], nums[i] = nums[i], nums[first]
-        dfs(0)
+        def helper(currIdx):
+            if currIdx == n:
+                out.append(nums.copy())
+            else:
+                for idx in range(currIdx, n):
+                    nums[currIdx], nums[idx] = nums[idx], nums[currIdx]
+                    helper(currIdx + 1)
+                    nums[currIdx], nums[idx] = nums[idx], nums[currIdx]
+        helper(0)
         return out
