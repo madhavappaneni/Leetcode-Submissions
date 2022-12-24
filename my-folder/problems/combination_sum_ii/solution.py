f459742dc2fcd0951245084fadefee96ee787c80
Madhav Appaneni <appanenim@gmail.com>
2022-12-24 01:55:38 +0000

    Sync LeetCode submission - Combination Sum II (python3)

--- my-folder/problems/combination_sum_ii/solution.py
+++ my-folder/problems/combination_sum_ii/solution.py
@@ -1,23 +1,17 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
         out = []
-        def dfs(rem, path, idx):
-            if rem == 0:
-                out.append(path[:])
+        candidates.sort()
+        def helper(currArr, remTarget, currIndex):
+            if remTarget < 0:
                 return
-            elif rem < 0:
-                return
-
-            for i in range(idx, len(candidates)):
-                if i > idx and candidates[i] == candidates[i-1]:
+            elif remTarget == 0:
+                out.append(currArr.copy())
+            for i in range(currIndex, len(candidates)):
+                if candidates[i] == candidates[i-1] and i != currIndex:
                     continue
-                path.append(candidates[i])
-                dfs(rem - candidates[i], path, i + 1)
-                path.pop()
-                # dfs(rem, path, i + 1)
-                
-        dfs(target, [], 0)
-        
+                currArr.append(candidates[i])
+                helper(currArr, remTarget - candidates[i], i + 1)
+                currArr.pop()
+        helper([], target, 0)
         return out
-                
