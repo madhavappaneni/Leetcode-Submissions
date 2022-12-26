916e33142fbe7a1b58aec29bfdef532298c7da28
Madhav Appaneni <appanenim@gmail.com>
2022-12-26 01:24:55 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- my-folder/problems/climbing_stairs/solution.py
+++ my-folder/problems/climbing_stairs/solution.py
@@ -1,10 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = [0] * (n + 1)
-        if n == 1:
-            return 1
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n + 1):
-            dp[i] = dp[i-2] + dp[i-1]
-        return max(dp)
+        mem = [0, 1]
+        if n == 0 or n == 1 or n ==2:
+            return n
+
+        for i in range(2, n + 1):
+            mem[i % 2] = sum(mem)
+        return sum(mem)
+
