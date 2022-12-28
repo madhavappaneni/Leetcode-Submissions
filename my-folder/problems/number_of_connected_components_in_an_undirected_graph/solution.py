de31de44a14ababe32b8266950ea85120bbc118b
Madhav Appaneni <appanenim@gmail.com>
2022-12-28 18:43:57 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (python3)

--- my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
+++ my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
@@ -1,24 +1,27 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+    
+    def find(self, x):
+        return self.root[x]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            for i in range(len(self.root)):
+                if self.root[i] == rootX:
+                    self.root[i] = rootY
+        return
+    
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+        
+
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        graph = [[] for _ in range(n)]
-        visited = [0] * n
-        
-        for firstEdge, secondEdge in edges:
-            graph[firstEdge].append(secondEdge)
-            graph[secondEdge].append(firstEdge)
+        uf = UnionFind(n)
+        for edge in edges:
+            uf.union(edge[0], edge[1])
+        return len(set(uf.root))
             
-        def dfs(idx):
-            for nei in graph[idx]:
-                if visited[nei] == 0:
-                    visited[nei] = 1
-                    dfs(nei)
-        # print(graph)
-        count = 0
-        for i in range(n):
-            # print(visited)
-            if visited[i] == 0 :
-                count += 1
-                visited[i] = 1
-                dfs(i)
-            
-        return count
