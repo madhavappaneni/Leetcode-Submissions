be9298955ccd1f6ed5b69549c5cf8a63a6512654
Madhav Appaneni <appanenim@gmail.com>
2023-01-07 00:56:02 +0000

    Sync LeetCode submission - House Robber (python3)

--- my-folder/problems/house_robber/solution.py
+++ my-folder/problems/house_robber/solution.py
@@ -1,11 +1,25 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        # Memoization        
+        # @lru_cache()
+        # def robHelper(idx):
+        #     if not 0 <= idx <= len(nums) - 1:
+        #         return 0
+        #     elif idx == 0:
+        #         return nums[0]
+        #     elif idx == 1:
+        #         return max(nums[0], nums[1])
+        #     else:
+        #         return max(robHelper(idx - 1), nums[idx] + robHelper(idx - 2))
+        
+        # return robHelper(len(nums) - 1)
+
         n = len(nums)
-        if n == 1:
-            return nums[0]
-        first, second = nums[0], max(nums[0], nums[1])
+        if (n) <= 2:
+            return max(nums)
+        dp = [0] * (n)
+        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+
         for i in range(2, n):
-            current = max(first + nums[i], second)
-            first = second
-            second = current
-        return second
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        return dp[-1]
