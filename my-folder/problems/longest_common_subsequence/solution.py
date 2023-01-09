7380f3ac50e8ca58f0ac229cbf68f906baaa9fe1
Madhav Appaneni <appanenim@gmail.com>
2023-01-09 22:57:00 +0000

    Sync LeetCode submission - Longest Common Subsequence (python3)

--- my-folder/problems/longest_common_subsequence/solution.py
+++ my-folder/problems/longest_common_subsequence/solution.py
@@ -1,20 +1,30 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        x = len(text1)
-        y = len(text2)
         
-        dp = [[-1] * (y + 1) for _ in range(x + 1)]
+#         @lru_cache(maxsize = None)
+#         def helper(i, j):
+#             if i == len(text1) or j == len(text2):
+#                 return 0
         
-        for i in range(x + 1):
-            for j in range(y + 1):
+#             if text1[i] == text2[j]:
+#                 return 1 + helper(i+1, j+1)
+#             return max(helper(i+1, j), helper(i, j+1))
+                
+#         return helper(0, 0)
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m+1)]
+        
+#         for i in range(m + 1):
+#             for j in range(n + 1):
+                
+                    
+        for i in range(m + 1):
+            for j in range(n + 1):
                 if i == 0 or j == 0:
                     dp[i][j] = 0
-        
-        for i in range(1, x + 1):
-            for j in range(1, y + 1):
-                if text1[i - 1] == text2[j - 1]:
-                    dp[i][j] = dp[i-1][j-1] + 1
+                elif text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        # print(dp)
         return dp[-1][-1]
-                    
