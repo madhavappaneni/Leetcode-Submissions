56943a6e77e4a72bc045cd4375ba7a6b2445b639
Madhav Appaneni <appanenim@gmail.com>
2023-01-09 20:46:32 +0000

    Sync LeetCode submission - N-th Tribonacci Number (python3)

--- my-folder/problems/n-th_tribonacci_number/solution.py
+++ my-folder/problems/n-th_tribonacci_number/solution.py
@@ -3,13 +3,13 @@ class Solution:
        
         if n == 0:
             return 0
-    
         if n == 1 or n == 2:
             return 1
+        
         dp = [0] * (n + 1)
         dp[0], dp[1], dp[2] = 0, 1, 1
         
         for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
-
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+        
         return dp[-1]
