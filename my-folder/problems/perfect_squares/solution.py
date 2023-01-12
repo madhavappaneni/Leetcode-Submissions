65c5da2bc5ccf318e1868e6e473443361eaf8942
Madhav Appaneni <appanenim@gmail.com>
2023-01-12 22:22:25 +0000

    Sync LeetCode submission - Perfect Squares (python3)

---
+++ my-folder/problems/perfect_squares/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+
+        dp = [float('inf')] * (n + 1)
+
+        squares = [i ** 2 for i in range(int(sqrt(n)) + 1)]
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(n + 1):
+            for square in squares:
+                if i - square >= 0:
+                    dp[i] = min(dp[i], dp[i-square] + 1)
+        # print(dp)
+        return dp[-1]
+            
