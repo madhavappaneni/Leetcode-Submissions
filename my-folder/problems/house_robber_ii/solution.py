604dad0f05c4839bab95d74b9879713dc552c5c6
Madhav Appaneni <appanenim@gmail.com>
2023-01-12 20:58:09 +0000

    Sync LeetCode submission - House Robber II (python3)

--- my-folder/problems/house_robber_ii/solution.py
+++ my-folder/problems/house_robber_ii/solution.py
@@ -1,25 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n == 1:
-            return nums[0]
-        firstMax, secondMax = 0, 0
-        dp = [0] * (n + 1)
-        first, second = nums[0], max(nums[0], nums[1])
-        
-        for i in range(2, n-1):
-            current = max(first + nums[i], second)
-            first = second
-            second = current
-        firstMax = second
-        
-        if n > 2:
-            first, second = nums[1], max(nums[1], nums[2])
-            for i in range(3, n):
-                current = max(first + nums[i], second)
-                first = second
-                second = current
+        if len(nums) <= 2:
+            return max(nums)
 
-            secondMax = second
-    
-        return max(firstMax, secondMax)
+        def robSimple(houses):
+            n = len(houses)
+            dp = [0] * n
+            if n <= 2:
+                return max(houses)
+
+            dp[0], dp[1] = houses[0], max(houses[0], houses[1])
+            for i in range(2, n):
+                dp[i] = max(dp[i-1], dp[i-2] + houses[i])
+            print(dp)
+            return max(dp)
+        
+        return max(robSimple(nums[1:]), robSimple(nums[:-1]))
