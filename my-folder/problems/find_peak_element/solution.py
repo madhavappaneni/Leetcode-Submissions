e471810e1401926518ff91dadb6e1b8ecd826ddd
Madhav Appaneni <appanenim@gmail.com>
2023-01-13 22:48:59 +0000

    Sync LeetCode submission - Find Peak Element (python3)

--- my-folder/problems/find_peak_element/solution.py
+++ my-folder/problems/find_peak_element/solution.py
@@ -1,31 +1,39 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        start, end = 0, len(nums) - 1
+        # low, high = 0, len(nums) - 1
+        # if len(nums) == 1:
+        #     return 0
+        # while low <= high:
+        #     mid = (low + high) >> 1
 
-        if len(nums) == 1:
-            return 0
+        #     if mid > 0 and mid < len(nums) - 1:
+        #         if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]:
+        #             return mid
+                
+        #         elif nums[mid] < nums[mid + 1]:
+        #             low = mid + 1
+                
+        #         else:
+        #             high = mid - 1
+            
+        #     elif mid == 0:
+        #         if nums[mid] > nums[mid + 1]:
+        #             return mid
+        #         else:
+        #             low = mid + 1
+        #     elif mid == len(nums) - 1:
+        #         if nums[mid] > nums[mid - 1]:
+        #             return mid
+        #         else:
+        #             high = mid - 1
+        # return mid
 
-        while start <= end:
-            mid = start + (end - start) // 2
-            if mid > 0 and mid < len(nums) - 1:
-                if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
-                    return mid
+        low, high = 0, len(nums) - 1
 
-                elif nums[mid - 1] > nums[mid]:
-                    end = mid - 1
-
-                else:
-                    start = mid + 1
-            elif mid == 0:
-                if nums[mid] > nums[mid + 1]:
-                    return mid
-                else:
-                    start = mid + 1
-
-            elif mid == len(nums) - 1:
-                if nums[mid] > nums[mid - 1]:
-                    return mid
-                else:
-                    end = mid - 1
-
-        return mid
+        while low < high:
+            mid = (low + high) >> 1
+            if nums[mid] < nums[mid + 1]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
