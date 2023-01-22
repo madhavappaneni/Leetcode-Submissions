b3b357752dd71a6eff71a6fdc7e729fe850093ac
Madhav Appaneni <appanenim@gmail.com>
2023-01-22 06:36:45 +0000

    Sync LeetCode submission - Maximal Square (python)

--- my-folder/problems/maximal_square/solution.py
+++ my-folder/problems/maximal_square/solution.py
@@ -1,12 +1,16 @@
-class Solution:
-    def maximalSquare(self, matrix: List[List[str]]) -> int:
+class Solution(object):
+    def maximalSquare(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
         m, n = len(matrix), len(matrix[0])
-
-        dp = [[0] * (n+1) for _ in range(m+1)]
-        maxEdge = 0
+        dp = [[0] * (n + 1) for i in range(m + 1)]
+        maxVal = float("-inf")
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if matrix[i-1][j-1] == '1':
                     dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
-                    maxEdge = max(maxEdge, dp[i][j])
-        return maxEdge * maxEdge
+                    maxVal = max(maxVal, dp[i][j])
+        return maxVal ** 2 if maxVal != float('-inf') else 0
+                
