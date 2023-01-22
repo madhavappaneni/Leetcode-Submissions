48c75c30087be82eda8bd344c3cfa7d4d06e5f83
Madhav Appaneni <appanenim@gmail.com>
2023-01-22 05:16:34 +0000

    Sync LeetCode submission - Counting Bits (python)

--- my-folder/problems/counting_bits/solution.py
+++ my-folder/problems/counting_bits/solution.py
@@ -1,3 +1,16 @@
-class Solution:
-    def countBits(self, n: int) -> List[int]:
-        return [str(bin(i)).count('1') for i in range(0, n+1)]
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        # def getOneBits(n):
+            # count = 0
+            # while n:
+            #     n = n & (n-1)
+            #     count += 1
+            # return count
+        ans = [0] 
+        for i in range(1, n + 1):
+            ans.append(ans[i & (i-1)] + 1)
+        return ans
