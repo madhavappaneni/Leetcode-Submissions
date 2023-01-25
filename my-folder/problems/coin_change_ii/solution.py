33ad75cb1ecf2eda5e6f17780c34413dc0988298
Madhav Appaneni <appanenim@gmail.com>
2023-01-25 01:24:33 +0000

    Sync LeetCode submission - Coin Change II (python3)

--- my-folder/problems/coin_change_ii/solution.py
+++ my-folder/problems/coin_change_ii/solution.py
@@ -1,27 +1,20 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        mem = [[-1] * (amount + 1) for _ in range(len(coins) + 1)]
         
-        # for i in range(len(coins) + 1):
-            # mem[i][0] = 1
+        n = len(coins)
         
-        # print(mem)
-        def helper(target, idx):
-            if mem[idx][target] != -1:
-                return mem[idx][target]
+        @lru_cache(None)
+        def solve(amount, idx):
+            if idx >= n:
+                return 0
             
-            if target == 0:
-                mem[idx][target] = 1 
-                return mem[idx][target]
-            if idx > len(coins) - 1:
-                mem[idx][target] = 0
-                return mem[idx][target]
-            out = 0
-            if coins[idx] <= target:
-                out += helper(target - coins[idx], idx)
-                out += helper(target, idx + 1)
-            else:
-                out += helper(target, idx + 1)
-            mem[idx][target] = out
-            return mem[idx][target]
-        return helper(amount, 0)
+            if amount == 0:
+                return 1
+            
+            if amount < 0:
+                return 0
+            
+            return solve(amount - coins[idx], idx) + solve(amount, idx + 1)
+        
+        return solve(amount, 0)
+                
