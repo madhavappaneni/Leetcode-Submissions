e4fb0adde8a0633f6c434dd6802d41e987df6cf9
Madhav Appaneni <appanenim@gmail.com>
2023-01-26 00:30:28 +0000

    Sync LeetCode submission - Valid Sudoku (python3)

--- my-folder/problems/valid_sudoku/solution.py
+++ my-folder/problems/valid_sudoku/solution.py
@@ -1,32 +1,27 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        N = 9
-        rows = [set() for _ in range(N)]
-        cols = [set() for _ in range(N)]
-        boxes = [set() for _ in range(N)]
+        def isBlockValid(block):
+            block = [i for i in block if i != '.']
+            return len(block) == len(set(block))
+            
+        def areRowsValid():
+            for row in board:
+                if not isBlockValid(row):
+                    return False
+            return True
+
+        def areColsValid():
+            for col in zip(*board):
+                if not isBlockValid(col):
+                    return False
+            return True
+
+        def areSubBoardsValid():
+            for i in range(0, 7, 3):
+                for j in range(0, 7, 3):
+                    subBoard = [board[m][n] for m in range(i, i + 3) for n in range(j, j + 3)]
+                    if not isBlockValid(subBoard):
+                        return False
+            return True
         
-        for r in range(9):
-            for c in range(9):
-                val = board[r][c]
-                
-                if val == '.':
-                    continue
-                
-                if val in rows[r]:
-                    return False
-                else:
-                    rows[r].add(val)
-                
-                if val in cols[c]:
-                    return False
-                else:
-                    cols[c].add(val)
-                
-                idx = (r // 3) * 3 + c // 3
-                
-                if val in boxes[idx]:
-                    return False
-                else:
-                    boxes[idx].add(val)
-                
-        return True
+        return areRowsValid() and areColsValid() and areSubBoardsValid()
