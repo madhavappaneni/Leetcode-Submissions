860683ba7c633683104af208f18c69e1833fdbb9
Madhav Appaneni <appanenim@gmail.com>
2023-01-28 19:23:20 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (python3)

--- my-folder/problems/best_time_to_buy_and_sell_stock/solution.py
+++ my-folder/problems/best_time_to_buy_and_sell_stock/solution.py
@@ -1,14 +1,41 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        largestDiff = float("-inf")
-        leastIndex = 0
-        i = 0
+        # bruteforce
+        # maxProfit = float('-inf')
+        # for i in range(len(prices)):
+        #     for j in range(i + 1, len(prices)):
+        #         maxProfit = max(maxProfit, prices[j] - prices[i])
         
-        for i in range(1, len(prices)):
-            if prices[i] > prices[leastIndex]:
-                largestDiff = max(largestDiff, prices[i] - prices[leastIndex])
-            else:
-                leastIndex = i
-        return largestDiff if largestDiff > 0 else 0
+        # return maxProfit if maxProfit > 0 else 0
+
+
+        # one_pass
+        # minPrice = float('inf')
+        # maxProfit = float('-inf')
+        # for i in range(len(prices)):
+        #     if prices[i] < minPrice:
+        #         minPrice = prices[i]
             
-        
+        #     elif prices[i] - minPrice > maxProfit:
+        #         maxProfit = prices[i] - minPrice
+
+        # return maxProfit if maxProfit > 0 else 0
+
+
+        #   public int maxProfit(int[] prices) {
+        # int[] dp = new int[prices.length];
+        # dp[0] = 0;
+        # int min = prices[0];
+        # for (int i = 1; i < prices.length; i++) {
+        #     min = Math.min( min, prices[i]);
+        #     dp[i] = Math.max( dp[i-1],  prices[i] - min );
+        # }
+        # return dp[prices.length - 1];
+
+
+        dp = [0] * len(prices)
+        minVal = float('inf')
+        for i in range(len(prices)):
+            minVal = min(minVal, prices[i])
+            dp[i] = max(dp[i - 1], prices[i] - minVal)
+        return dp[-1]
