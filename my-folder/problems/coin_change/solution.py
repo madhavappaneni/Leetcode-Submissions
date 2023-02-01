8d06aa7815666c81db3b8356b7efd3dba4a2cfbe
Madhav Appaneni <appanenim@gmail.com>
2023-02-01 23:58:12 +0000

    Sync LeetCode submission - Coin Change (python3)

--- my-folder/problems/coin_change/solution.py
+++ my-folder/problems/coin_change/solution.py
@@ -1,28 +1,26 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # n = len(coins)
-
-        # def coinChangeHelper(remAmount):
-        #     if remAmount == 0:
-        #         return 0
+        # @lru_cache(None)
+        # def solve(remAmount):
         #     if remAmount < 0:
         #         return -1
-        #     mini = float('inf')
+        #     if remAmount == 0:
+        #         return 0
+        #     minCoins = float('inf')
+
         #     for coin in coins:
-        #         ans = coinChangeHelper(remAmount - coin)
-        #         if ans != -1:
-        #             mini = min(ans, mini)
-        #     return mini + 1 if mini != float(inf) else -1
-        
-        # return coinChangeHelper(amount)
+        #         if remAmount >= coin:
+        #             numCoins = solve(remAmount - coin)
+        #             if numCoins != -1:
+        #                 minCoins = min(minCoins, numCoins + 1)
+        #     return minCoins if minCoins != float('inf') else -1
+        # return solve(amount)
 
-        dp = [float(inf)] * (amount + 1)
+        dp = [float('inf')] * (amount + 1)
         dp[0] = 0
-        for i in range(1, amount + 1):
+        for i in range(amount + 1):
             for coin in coins:
-                if i - coin >= 0:
-                    dp[i] = min(dp[i-coin] + 1, dp[i])
-
+                if i >= coin:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
+            
         return dp[-1] if dp[-1] != float('inf') else -1
-
-
