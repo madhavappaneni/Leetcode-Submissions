0503a9fbaa107bfbabb9089659012a211510d96f
Madhav Appaneni <appanenim@gmail.com>
2023-02-13 02:21:33 +0000

    Sync LeetCode submission - Two Sum (python3)

--- my-folder/problems/two_sum/solution.py
+++ my-folder/problems/two_sum/solution.py
@@ -1,9 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
         
-        for i in range(len(nums)):
-            if target-nums[i] not in hashmap:
-                hashmap[nums[i]] = i
-            else:
-                return [i, hashmap[target-nums[i]]]
+        # h = defaultdict(int)
+
+        # for idx, n in enumerate(nums):
+        #     if target - n in h:
+        #         return idx, h[target-n]
+        #     else:
+        #         h[n] = idx
+
+        for i in range(0, len(nums) + 1):
+            diff = target-nums[i]
+            if diff in nums and not i == nums.index(diff):
+                return [i, nums.index(diff)]
