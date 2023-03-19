065c2f302f6ac908ae6efc69c6fef9ac260a4e72
Madhav Appaneni <appanenim@gmail.com>
2023-03-19 01:07:53 +0000

    Sync LeetCode submission - Palindrome Linked List (python3)

--- my-folder/problems/palindrome_linked_list/solution.py
+++ my-folder/problems/palindrome_linked_list/solution.py
@@ -4,20 +4,15 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    curr = None
-    
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        self.isPalin = True
-        
         self.curr = head
-        
-        def helper(currRec):
-            if currRec:
-                helper(currRec.next)
-                # print(currRec.val,self.curr.val)
-                if currRec.val != self.curr.val:
-                    self.isPalin = False
-                self.curr = self.curr.next
 
-        helper(head)
-        return self.isPalin
+        def palindromeHelper(node):
+            if node:
+                if not palindromeHelper(node.next):
+                    return False
+                if node.val != self.curr.val:
+                    return False
+                self.curr = self.curr.next
+            return True
+        return palindromeHelper(head)
