74a5142b2560f977b165ef6a1ff951df6ab7f10f
Madhav Appaneni <appanenim@gmail.com>
2023-04-06 21:04:37 +0000

    Sync LeetCode submission - Clone Graph (python3)

--- my-folder/problems/clone_graph/solution.py
+++ my-folder/problems/clone_graph/solution.py
@@ -9,15 +9,16 @@ class Node:
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         visited = {}
-        newNode = Node(None)
-        def dfs(curr):
-            if not curr:
-                return
-            if curr in visited:
-                return visited[curr]
-            newNode = Node(curr.val, [])
-            for i in (curr.neighbors):
-                visited[curr] = newNode
-                newNode.neighbors = [dfs(nei) for nei in curr.neighbors]
-            return newNode
+
+        def dfs(node):
+            if not node:
+                return node
+            if node in visited:
+                return visited[node]
+            currNode = Node(node.val, [])
+            visited[node] = currNode
+            if node.neighbors:
+                currNode.neighbors = [dfs(nei) for nei in node.neighbors]
+            return currNode
         return dfs(node)
+        
