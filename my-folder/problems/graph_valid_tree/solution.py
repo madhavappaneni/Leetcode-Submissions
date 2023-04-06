7add3234fcf0133f77e5dc20ddd03237a7c94295
Madhav Appaneni <appanenim@gmail.com>
2023-04-06 02:34:03 +0000

    Sync LeetCode submission - Graph Valid Tree (python3)

--- my-folder/problems/graph_valid_tree/solution.py
+++ my-folder/problems/graph_valid_tree/solution.py
@@ -1,31 +1,23 @@
-class UnionFind:
-    def __init__(self, size):
-        self.parent = [i for i in range(size)]
-    
-    def find(self, x):
-        while x != self.parent[x]:
-            x = self.parent[x]
-        return x
-    
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            self.parent[rootX] = rootY
-            return True
-        else:
-            return False
-    
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-        
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        uf = UnionFind(n)
-        if len(edges) != n - 1: return False
-        for edge in edges:
-            if not uf.union(edge[0], edge[1]):
-                return False
-        return True
+        if len(edges) != n - 1:
+            return False
         
+        adj_list = collections.defaultdict(list)
+
+        for start, end in edges:
+            adj_list[start].append(end)
+            adj_list[end].append(start)
+        
+        seen = set()
+
+        def dfs(node):
+            if node in seen:
+                return 
+            seen.add(node)
+            for nei in adj_list[node]:
+                dfs(nei)
+
+        dfs(0)
+
+        return len(seen) == n
