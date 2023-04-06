b6e9f8214e7390dda3a9e08bccbeb0ae0112f8ed
Madhav Appaneni <appanenim@gmail.com>
2023-04-06 19:23:34 +0000

    Sync LeetCode submission - Find if Path Exists in Graph (python3)

--- my-folder/problems/find_if_path_exists_in_graph/solution.py
+++ my-folder/problems/find_if_path_exists_in_graph/solution.py
@@ -1,40 +1,14 @@
-       
 class Solution:
-    # def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-    #     g = collections.defaultdict(list)
-    #     for edge in edges:
-    #         g[edge[0]].append(edge[1])
-    #         g[edge[1]].append(edge[0])
-    #     visited = set()
-    #     def helper(curr):
-    #         if curr in visited:
-    #             return
-    #         elif curr == destination:
-    #             return True
-    #         else:
-    #             visited.add(curr)
-    #             for nei in g[curr]:
-    #                 if helper(nei):
-    #                     return True
-    #         return False
-    #     return helper(source)
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        g = collections.defaultdict(list)
-        for edge in edges:
-            g[edge[0]].append(edge[1])
-            g[edge[1]].append(edge[0])
-        seen = [False] * n
-        seen[source] = True
-        deq = collections.deque([source])
-        while deq:
-            elem = deq.popleft()
-            if elem == destination:
-                return True
-            for nei in g[elem]:
-                if not seen[nei]:
-                    seen[nei] = True
-                    deq.append(nei)
-        return False
+        adjList = collections.defaultdict(list)
+        for u, v in edges:
+            adjList[u].append(v)
+            adjList[v].append(u)
+        seen = set()
+        def dfs(node):
+            if node in seen: return False
+            if node == destination: return True
+            seen.add(node)
+            return any([dfs(nei) for nei in adjList[node]])
         
-
-        
+        return dfs(source)
