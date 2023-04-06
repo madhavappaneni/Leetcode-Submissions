e371fd5f327d2f7ca5028fcfc48c27ffad1497d7
Madhav Appaneni <appanenim@gmail.com>
2023-04-06 02:40:46 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (python3)

--- my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
+++ my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
@@ -1,27 +1,24 @@
-class UnionFind:
-    def __init__(self, size):
-        self.root = [i for i in range(size)]
-    
-    def find(self, x):
-        return self.root[x]
-    
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            for i in range(len(self.root)):
-                if self.root[i] == rootX:
-                    self.root[i] = rootY
-        return
-    
-    def connected(self, x, y):
-        return self.find(x) == self.find(y)
-        
-
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        uf = UnionFind(n)
-        for edge in edges:
-            uf.union(edge[0], edge[1])
-        return len(set(uf.root))
+        seen = set()
+        adjList = collections.defaultdict(list)
+
+        for u, v in edges:
+            adjList[u].append(v)
+            adjList[v].append(u)
+        
+        def dfs(node):
+            if node in seen:
+                return 
+            seen.add(node)
+            for nei in adjList[node]:
+                dfs(nei)
             
+        count = 0
+
+        for i in range(n):
+            if i not in seen:
+                dfs(i)
+                count += 1
+
+        return count
