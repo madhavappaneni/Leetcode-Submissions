a877bc934865261f295662453b1b613afbebe00f
Madhav Appaneni <appanenim@gmail.com>
2023-05-19 23:57:28 +0000

    Sync LeetCode submission - Encode and Decode Strings (python3)

--- my-folder/problems/encode_and_decode_strings/solution.py
+++ my-folder/problems/encode_and_decode_strings/solution.py
@@ -3,15 +3,31 @@ class Codec:
         """Encodes a list of strings to a single string.
         """
         out = ""
+
         for s in strs:
-            out += s
-            out += "/|\\"
+            out += str(len(s)) + "#" + s
         return out
+        
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
-        return s.split("/|\\")[:-1]
+        out, i = [], 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            out.append(s[j + 1: j + length + 1])
+            i = j + 1 + length
+        return out
+            
+
+
+
+
+        
 
 
 # Your Codec object will be instantiated and called as such:
