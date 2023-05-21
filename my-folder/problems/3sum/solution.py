d9f83f01638ed2b558ade06c24dcbad0d2aae1e6
Madhav Appaneni <appanenim@gmail.com>
2023-05-21 01:53:22 +0000

    Sync LeetCode submission - 3Sum (python3)

--- my-folder/problems/3sum/solution.py
+++ my-folder/problems/3sum/solution.py
@@ -1,21 +1,20 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
-        ans = []
+        res = []
         for pivot in range(len(nums)):
-            if pivot > 0 and nums[pivot] == nums[pivot - 1]:
+            if pivot > 0 and nums[pivot] == nums[pivot-1]:
                 continue
-            target = -1 * nums[pivot]
-            start, end = pivot + 1, len(nums) - 1
-            while start < end:
-                if nums[start] + nums[end] == target:
-                    ans.append([nums[pivot], nums[start], nums[end]])
-                    start += 1
-                    end -= 1
-                    while nums[start] == nums[start - 1] and start < end:
-                        start += 1
-                elif nums[start] + nums[end] > target:
-                    end -= 1
-                else:
-                    start += 1
-        return ans
+            h = {}
+            i = pivot + 1
+            while i < len(nums):
+                complement =  -1 * nums[pivot] - nums[i]
+
+                if complement in h:
+                    res.append([nums[pivot], nums[i], complement])
+                    while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                        i += 1
+                h[nums[i]] = i
+                i += 1
+        return res
+            
