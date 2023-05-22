4f02a5dd23ac2953545ecb5b20f37093fa28b8c1
Madhav Appaneni <appanenim@gmail.com>
2023-05-22 00:53:01 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock (python3)

--- my-folder/problems/best_time_to_buy_and_sell_stock/solution.py
+++ my-folder/problems/best_time_to_buy_and_sell_stock/solution.py
@@ -1,41 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # bruteforce
-        # maxProfit = float('-inf')
+        # maxprofit = float('-inf')
         # for i in range(len(prices)):
         #     for j in range(i + 1, len(prices)):
-        #         maxProfit = max(maxProfit, prices[j] - prices[i])
+        #         profit = (prices[j] - prices[i])
+        #         maxprofit = max(maxprofit, profit)
         
-        # return maxProfit if maxProfit > 0 else 0
+        # return maxprofit if maxprofit > 0 else 0
 
+        # onepass
+        maxprofit = float('-inf')
+        minValue = float('inf')
 
-        # one_pass
-        # minPrice = float('inf')
-        # maxProfit = float('-inf')
-        # for i in range(len(prices)):
-        #     if prices[i] < minPrice:
-        #         minPrice = prices[i]
-            
-        #     elif prices[i] - minPrice > maxProfit:
-        #         maxProfit = prices[i] - minPrice
-
-        # return maxProfit if maxProfit > 0 else 0
-
-
-        #   public int maxProfit(int[] prices) {
-        # int[] dp = new int[prices.length];
-        # dp[0] = 0;
-        # int min = prices[0];
-        # for (int i = 1; i < prices.length; i++) {
-        #     min = Math.min( min, prices[i]);
-        #     dp[i] = Math.max( dp[i-1],  prices[i] - min );
-        # }
-        # return dp[prices.length - 1];
-
-
-        dp = [0] * len(prices)
-        minVal = float('inf')
         for i in range(len(prices)):
-            minVal = min(minVal, prices[i])
-            dp[i] = max(dp[i - 1], prices[i] - minVal)
-        return dp[-1]
+
+            if prices[i] > minValue:
+                maxprofit = max(maxprofit, prices[i] - minValue)
+            else:
+                minValue = min(minValue, prices[i])
+        
+        return maxprofit if maxprofit > 0 else 0
