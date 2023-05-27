5b132eb2150427fda9776a53ed59fe291bda0328
Madhav Appaneni <appanenim@gmail.com>
2023-05-27 20:42:44 +0000

    Sync LeetCode submission - Reorder List (python3)

--- my-folder/problems/reorder_list/solution.py
+++ my-folder/problems/reorder_list/solution.py
@@ -8,29 +8,26 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        dummy = ListNode()
-        dummy.next = head
-        fast = slow = dummy.next
-        
+        slow, fast = head, head.next
+
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
+        
+        second = slow.next
+        prev = slow.next = None
 
-        curr = slow
-        prev = None
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+
         first, second = head, prev
-        
-        while second.next:
-            tmp = first.next
+
+        while second:
+            tmp1, tmp2 = first.next, second.next
             first.next = second
-            first = tmp
-            
-            tmp = second.next
-            second.next = first
-            second = tmp
+            second.next = tmp1
+            first = tmp1
+            second = tmp2
