efd5076969a776376d2b4ef6873172534eb989ef
Madhav Appaneni <appanenim@gmail.com>
2023-05-30 22:15:33 +0000

    Sync LeetCode submission - Validate Binary Search Tree (python3)

--- my-folder/problems/validate_binary_search_tree/solution.py
+++ my-folder/problems/validate_binary_search_tree/solution.py
@@ -6,26 +6,13 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # self.prev = float('-inf')
-        # def inorder(node):
-        #     global prev
-
-        #     if not node:
-        #         return True
-        #     if not inorder(node.left):
-        #         return False
-        #     # print(prev, node.val)
-        #     if self.prev >= node.val:
-        #         return False
-        #     self.prev = node.val
-        #     return inorder(node.right)
         
-        # return inorder(root)
-
         def helper(node, minValue, maxValue):
             if not node:
                 return True
-            elif node.val <= minValue or node.val >= maxValue:
-                return False
-            return helper(node.right, node.val, maxValue) and helper(node.left, minValue, node.val)
+
+            return (minValue < node.val < maxValue) \
+                and helper(node.left, minValue, node.val)\
+                and helper(node.right, node.val, maxValue)
+        
         return helper(root, float('-inf'), float('inf'))
