fd49435098a235d514a745998d8a3bc016b23e9e
Madhav Appaneni <appanenim@gmail.com>
2023-06-02 22:05:17 +0000

    Sync LeetCode submission - Task Scheduler (python3)

--- my-folder/problems/task_scheduler/solution.py
+++ my-folder/problems/task_scheduler/solution.py
@@ -1,18 +1,58 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
+        queue = deque([])
         c = Counter(tasks)
-        frequencies = list(c.values())
-        frequencies.sort()
-        f_max = frequencies.pop()
-        idleTime = (f_max - 1) * n
+        heap = [-value for value in c.values()]
+        heapify(heap)
 
-        while frequencies and idleTime > 0:
-            idleTime -= min(f_max - 1, frequencies.pop())
-            # if freq.pop == fmax, take fmax - 1, else take freq.pop. 
-            # A _ _ A _ _ A _ _ A
-            # Consider B frequency is 4, it will take only 3 idle spaces.
-            # A B _ A B _ A B _ A B - took only 3 of idle spaces
-            # If B's frequency is < 4, it will take f idle spaces. 
-        idleTime = max(idleTime, 0)
+        time = 0
 
-        return len(tasks) + idleTime
+        while heap or queue:
+            time += 1
+
+            if not heap:
+                time = queue[0][1]
+            else:
+                cnt = 1 + heappop(heap)
+                if cnt:
+                    queue.append([cnt, time + n])
+                
+            if queue and queue[0][1] == time:
+                heappush(heap, queue.popleft()[0])
+                
+
+        return time
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def leastInterval(self, tasks: List[str], n: int) -> int:
+    #     c = Counter(tasks)
+    #     frequencies = list(c.values())
+    #     frequencies.sort()
+    #     f_max = frequencies.pop()
+    #     idleTime = (f_max - 1) * n
+
+    #     while frequencies and idleTime > 0:
+    #         idleTime -= min(f_max - 1, frequencies.pop())
+    #         # if freq.pop == fmax, take fmax - 1, else take freq.pop. 
+    #         # A _ _ A _ _ A _ _ A
+    #         # Consider B frequency is 4, it will take only 3 idle spaces.
+    #         # A B _ A B _ A B _ A B - took only 3 of idle spaces
+    #         # If B's frequency is < 4, it will take f idle spaces. 
+    #     idleTime = max(idleTime, 0)
+
+    #     return len(tasks) + idleTime
