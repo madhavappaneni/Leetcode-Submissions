38d822554bfdbe9d96247e6c8cede5435a4e8070
Madhav Appaneni <appanenim@gmail.com>
2023-06-03 05:15:32 +0000

    Sync LeetCode submission - Sort an Array (python3)

--- my-folder/problems/sort_an_array/solution.py
+++ my-folder/problems/sort_an_array/solution.py
@@ -1,7 +1,31 @@
-class Solution(object):
-    def sortArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        return sorted(nums)
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+
+        def mergeSort(nums):
+            if len(nums) <= 1:
+                return nums
+
+            pivot = int(len(nums) / 2)
+
+            left = mergeSort(nums[:pivot])
+            right = mergeSort(nums[pivot:])
+
+            return merge(left, right)
+        
+        def merge(left, right):
+            l, r = 0, 0
+            out = []
+            while l < len(left) and r < len(right):
+                if left[l] < right[r]:
+                    out.append(left[l])
+                    l += 1
+                else:
+                    out.append(right[r])
+                    r += 1
+            
+            out.extend(left[l:])
+            out.extend(right[r:])
+            
+            return out
+        
+        return mergeSort(nums)
