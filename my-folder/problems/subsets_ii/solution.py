b971782168c06a48491533a4b2f703bfdb6d9242
Madhav Appaneni <appanenim@gmail.com>
2023-06-04 23:16:41 +0000

    Sync LeetCode submission - Subsets II (python3)

--- my-folder/problems/subsets_ii/solution.py
+++ my-folder/problems/subsets_ii/solution.py
@@ -5,10 +5,12 @@ class Solution:
         def helper(currentIndex, currArr):
             out.append(currArr.copy())
             for i in range(currentIndex, len(nums)):
-                if i != currentIndex and nums[i] == nums[i-1]:
+                if i != currentIndex and nums[i] == nums[i - 1]:
                     continue
                 currArr.append(nums[i])
                 helper(i + 1, currArr)
                 currArr.pop()
+                    
         helper(0, [])
+    
         return out
