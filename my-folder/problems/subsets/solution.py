c9f928269e2210fff85a10fde5bffdc04141e910
Madhav Appaneni <appanenim@gmail.com>
2023-06-04 21:31:30 +0000

    Sync LeetCode submission - Subsets (python3)

--- my-folder/problems/subsets/solution.py
+++ my-folder/problems/subsets/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        output = []
-        def helper(counter, prefix):
-            # print('test', counter, prefix)
-            output.append(prefix.copy())
-            for i in range(counter, len(nums)):
-                prefix.append(nums[i])
-                helper(i + 1, prefix)
-                prefix.pop()
+        
+        out = []
 
+        def helper(currentIdx, currentPrefix):
+            out.append(currentPrefix.copy())
+
+            for i in range(currentIdx, len(nums)):
+                helper(i + 1, currentPrefix + [nums[i]])
+            
             return
+        
         helper(0, [])
-        return output
+
+        return out
