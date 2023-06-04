16461e4c70f2b16a17036137b2b816f2f745c462
Madhav Appaneni <appanenim@gmail.com>
2023-06-04 22:50:36 +0000

    Sync LeetCode submission - Combination Sum III (python3)

--- my-folder/problems/combination_sum_iii/solution.py
+++ my-folder/problems/combination_sum_iii/solution.py
@@ -1,16 +1,23 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         out = []
-        def helper(target, currIndex, k, currArr):
-            if target < 0 or k < 0:
+
+        def backtrack(currIdx, currPrefix, remSum):
+            if remSum == 0 and len(currPrefix) == k:
+                out.append(currPrefix[:])
+            
+            elif remSum < 0 or len(currPrefix) == k:
                 return
-            elif target == 0 and k == 0:
-                out.append(currArr.copy())
-            else:
-                for idx in range(currIndex, 10):
-                    currArr.append(idx)
-                    helper(target - idx, idx + 1, k - 1, currArr)
-                    currArr.pop()
-        helper(n, 1, k, [])
-        return out
+
+            for i in range(currIdx + 1, 10):
+                currPrefix.append(i)
+                backtrack(i, currPrefix, remSum - i)
+                currPrefix.pop()
+            
+            return
         
+        backtrack(0, [], n)
+
+        return out
+
+
