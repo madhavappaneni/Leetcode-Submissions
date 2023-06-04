6e2b1e86f29f4c35d35042867a4879c5e34a7f10
Madhav Appaneni <appanenim@gmail.com>
2023-06-04 22:20:37 +0000

    Sync LeetCode submission - Combinations (python3)

--- my-folder/problems/combinations/solution.py
+++ my-folder/problems/combinations/solution.py
@@ -2,13 +2,21 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         out = []
 
-        def helper(first, curr):
-            if len(curr) == k:
-                out.append(curr[:])
-            else:
-                for idx in range(first, n + 1):
-                    curr.append(idx)
-                    helper(idx + 1, curr)
-                    curr.pop()
-        helper(1, [])
+        def helper(currIdx, currPrefix):
+            if len(currPrefix) == k:
+                out.append(currPrefix.copy())
+                return
+            
+            need = k - len(currPrefix)
+            remain = n - currIdx + 1
+            available = remain - need
+            
+            for i in range(currIdx + 1, currIdx + available + 1):
+                currPrefix.append(i)
+                helper(i, currPrefix)
+                currPrefix.pop()
+            return
+        
+        helper(0, [])
+    
         return out
