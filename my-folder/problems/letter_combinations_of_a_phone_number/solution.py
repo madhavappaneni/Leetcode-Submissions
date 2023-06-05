621e26e275046cc9b0c38230504fe7db1744d1f2
Madhav Appaneni <appanenim@gmail.com>
2023-06-05 20:10:42 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (python3)

--- my-folder/problems/letter_combinations_of_a_phone_number/solution.py
+++ my-folder/problems/letter_combinations_of_a_phone_number/solution.py
@@ -1,15 +1,19 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        letters = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        numberToLetterMap = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+
         out = []
-        def helper(currIdx, prefix):
-            if currIdx < 0 or currIdx > 9:
+
+        def helper(start, prefix):
+            if start == len(digits) and prefix:
+                out.append(prefix)
+            if start >= len(digits):
                 return
-            elif currIdx == len(digits):
-                if prefix != "":
-                    out.append(prefix)
-            else:
-                for letter in letters[digits[currIdx]]:
-                    helper(currIdx + 1, prefix + letter)
+            for i in numberToLetterMap[digits[start]]:
+                helper(start + 1, prefix + i)
+            
+            return
+        
         helper(0, "")
+
         return out
