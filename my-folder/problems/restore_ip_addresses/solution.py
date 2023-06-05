ff927ba266608ba05dd6d55fa8465ef2c5a94301
Madhav Appaneni <appanenim@gmail.com>
2023-06-05 19:57:29 +0000

    Sync LeetCode submission - Restore IP Addresses (python3)

--- my-folder/problems/restore_ip_addresses/solution.py
+++ my-folder/problems/restore_ip_addresses/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        out = set()
+        out = []
+        def backtrack(currIdx, prefix, count):
+            print(currIdx, prefix)
+            if currIdx == len(s) and count == 4:
+                out.append(prefix)
 
-        def isValidPart(p):
-            return p != "" and int(p) >= 0 and int(p) <= 255 and len(str(int(p))) == len(p)
-
-        def helper(start, curr, k):
-            if k == 0:
-                if len(curr) == len(s) + 3:
-                    # print(start, curr, k)
-                    out.add(curr)
-                return
-            for idx in range(start, len(s) + 1):
-                newStr = s[start: idx + 1]
-                if isValidPart(newStr):
-                    newInput = newStr if not curr else curr + "." + newStr
-                    helper(idx + 1, newInput, k - 1)
-        helper(0, "", 4)
+            for i in range(currIdx, len(s)):
+                subStr = s[currIdx:i + 1]
+                if 0 <= int(subStr) <= 255 and len(subStr) == len(str(int(subStr))):
+                    newPrefix = s[currIdx: i + 1] if not prefix else prefix + "." + s[currIdx: i + 1]
+                    backtrack(i + 1, newPrefix, count + 1)
+            
+            return
+        
+        backtrack(0, "", 0)
         return out
+
+                
