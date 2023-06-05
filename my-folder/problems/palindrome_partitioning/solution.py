50ebdf92752459302e663b651247bceb63b63209
Madhav Appaneni <appanenim@gmail.com>
2023-06-05 19:04:03 +0000

    Sync LeetCode submission - Palindrome Partitioning (python3)

--- my-folder/problems/palindrome_partitioning/solution.py
+++ my-folder/problems/palindrome_partitioning/solution.py
@@ -1,21 +1,19 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-
-        def isPalindrome(s):
-            return s == s[::-1]
-        
         out = []
-        def helper(start, curr):
-            if curr and not isPalindrome(curr[-1]):
-                return
-            if start == len(s):
-                # print(start, curr)
-                out.append(curr[:])
-            for i in range(start, len(s)):
-                curr.append(s[start:i + 1])
-                helper(i + 1, curr)
-                curr.pop()
-        # print(out)
-        helper(0, [])
-        return out
+
+        def backtrack(currIdx, prefix):
+            # print('func', currIdx, prefix)
+            if currIdx == len(s):
+                out.append(prefix.copy())
             
+            for i in range(currIdx, len(s)):
+                # print(i, s[currIdx:i + 1], s[currIdx:i + 1] == s[currIdx:i + 1][::-1])
+                if s[currIdx:i + 1] == s[currIdx:i + 1][::-1]:
+                    backtrack(i + 1, prefix + [s[currIdx:i + 1]])
+            
+            return
+        
+        backtrack(0, [])
+        return out
+                
