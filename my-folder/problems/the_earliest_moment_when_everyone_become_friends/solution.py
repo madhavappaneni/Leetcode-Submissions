b4d49c5b52920daa795f867f115a3ce0b5618b2a
Madhav Appaneni <appanenim@gmail.com>
2023-06-06 20:17:22 +0000

    Sync LeetCode submission - The Earliest Moment When Everyone Become Friends (python3)

--- my-folder/problems/the_earliest_moment_when_everyone_become_friends/solution.py
+++ my-folder/problems/the_earliest_moment_when_everyone_become_friends/solution.py
@@ -1,31 +1,44 @@
 class UnionFind:
     def __init__(self, size):
-        self.root = [i for i in range(size)]
-        self.count = size
-    
+        self.root = [None] * size
+        self.rank = [1] * size
+        for i in range(size):
+            self.root[i] = i
+        
     def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
         return self.root[x]
-
+    
+    def connected(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        return rootX == rootY
+    
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
         if rootX != rootY:
-            for i in range(len(self.root)):
-                if self.root[i] == rootY:
-                    self.root[i] = rootX
-            self.count -= 1
-        return self.count
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootY] > self.rank[rootX]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootX] = rootY
+                self.rank[rootY] += 1
 
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
-        logs.sort(key = lambda x: x[0])
         uf = UnionFind(n)
-        if len(logs) < n - 1:
-            return -1
-        for log in logs:
-            unionOut = uf.union(log[1], log[2])
-            if unionOut == 1:
-                return log[0]
+        logs.sort()
+        count = n
+        # Initially there are n groups
+        # If all groups are friends, in the end, there would be 1 group
+        for time, u, v in logs:
+            if not uf.connected(u, v):
+                uf.union(u,v)
+                count -= 1
+                if count == 1:
+                    return time
         return -1
-            
-        
