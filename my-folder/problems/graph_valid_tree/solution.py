d30177cc2ea08d09ad0842ff0d17fa796271424c
Madhav Appaneni <appanenim@gmail.com>
2023-06-06 19:30:12 +0000

    Sync LeetCode submission - Graph Valid Tree (python3)

--- my-folder/problems/graph_valid_tree/solution.py
+++ my-folder/problems/graph_valid_tree/solution.py
@@ -1,23 +1,67 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = [None] * size
+        self.rank = [1] * size
+        for i in range(size):
+            self.root[i] = i
+        
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def connected(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        return rootX == rootY
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootY] > self.rank[rootX]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootX] = rootY
+                self.rank[rootY] += 1
+
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) != n - 1:
-            return False
+        uf = UnionFind(n)
+        count = 0
+        for u,v in edges:
+            if uf.connected(u, v):
+                return False
+            count += 1
+            uf.union(u, v)
         
-        adj_list = collections.defaultdict(list)
+        return count == n - 1
 
-        for start, end in edges:
-            adj_list[start].append(end)
-            adj_list[end].append(start)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
-        seen = set()
-
-        def dfs(node):
-            if node in seen:
-                return 
-            seen.add(node)
-            for nei in adj_list[node]:
-                dfs(nei)
-
-        dfs(0)
-
-        return len(seen) == n
