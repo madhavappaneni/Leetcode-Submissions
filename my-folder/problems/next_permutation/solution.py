ad96caca6069429d62d23f0d57649a568d828107
Madhav Appaneni <appanenim@gmail.com>
2023-06-06 16:32:40 +0000

    Sync LeetCode submission - Next Permutation (python3)

--- my-folder/problems/next_permutation/solution.py
+++ my-folder/problems/next_permutation/solution.py
@@ -3,16 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        
         i = len(nums) - 2
-        
-        while i > 0 and nums[i+1] <= nums[i]:
+
+        while i >= 0 and nums[i + 1] <= nums[i]:
             i -= 1
-        if(i>=0):
-            j = len(nums) - 1
-            # print(j, i, nums[j])
-            while j >= 0 and nums[j] <= nums[i]:
-                j -= 1
-            nums[i], nums[j] = nums[j], nums[i]
         
-        nums[i+1:] = sorted(nums[i+1:])
-    
+        if i >= 0:
+            j = len(nums) - 1
+
+            while nums[i] >= nums[j]:
+                j -= 1
+            
+            print(i, j)
+            nums[i], nums[j] = nums[j], nums[i]
+
+        start, end = i + 1, len(nums) - 1
+
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+        
