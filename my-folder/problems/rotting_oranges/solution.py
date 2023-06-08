2b8694ce550f188e3e68053fc1d3e7652bb8ed57
Madhav Appaneni <appanenim@gmail.com>
2023-06-08 19:58:13 +0000

    Sync LeetCode submission - Rotting Oranges (python3)

--- my-folder/problems/rotting_oranges/solution.py
+++ my-folder/problems/rotting_oranges/solution.py
@@ -1,31 +1,49 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rotten = 0 
-        fresh = 0
-        time = 0
+        queue = deque([])
         ROWS, COLS = len(grid), len(grid[0])
-        deq = deque()
-        
-        for i in range(ROWS):
-            for j in range(COLS):
-                if grid[i][j] == 2:
-                    deq.append((i,j))
-                if grid[i][j] == 1:
+        fresh = 0
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 2:
+                    queue.append([r, c])
+                if grid[r][c] == 1:
                     fresh += 1
-        # print(deq)
-        while deq and fresh > 0:
-            for i in range(len(deq)):
-                row, col = deq.popleft()
-                directions = [(0,1), (1, 0), (0, -1), (-1, 0)]
-                for dr, dc in directions:
-                    r = row + dr
-                    c = col + dc
-                    # print(row, col, r, c, range(ROWS), range(COLS))
-                    if r in range(ROWS) and c in range(COLS) and grid[r][c] == 1:
-                        grid[r][c] = 2
-                        deq.append((r,c))
+        time = 0
+
+        while queue and fresh > 0:
+            q_len = len(queue)    
+
+            for i in range(q_len):
+                r, c = queue.popleft()
+                dirs = [[0,1], [0,-1], [-1,0], [1,0]]
+
+                for dr, dc in dirs:
+                    newR, newC = r + dr, c + dc
+                    if newR in range(ROWS) and newC in range(COLS) and grid[newR][newC] == 1:
+                        queue.append([newR, newC])
+                        grid[newR][newC] = 2
+                        queue.append((newR, newC))
                         fresh -= 1
-                # print(grid)
             time += 1
-        # print(deq, time, fresh, grid)
+    
         return time if fresh == 0 else -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
