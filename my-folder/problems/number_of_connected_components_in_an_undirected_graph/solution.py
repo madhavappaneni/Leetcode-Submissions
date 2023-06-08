c44953a5f997b10de2c961e5cd091747e0b8ff25
Madhav Appaneni <appanenim@gmail.com>
2023-06-08 20:13:42 +0000

    Sync LeetCode submission - Number of Connected Components in an Undirected Graph (python3)

--- my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
+++ my-folder/problems/number_of_connected_components_in_an_undirected_graph/solution.py
@@ -1,38 +1,26 @@
-class UnionFind:
-    def __init__(self, size):
-        self.root = [None] * size
-        self.rank = [1] * size
-        for i in range(size):
-            self.root[i] = i
-        
-    def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-    
-    def connected(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        return rootX == rootY
-    
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootY] > self.rank[rootX]:
-                self.root[rootX] = rootY
-            else:
-                self.root[rootX] = rootY
-                self.rank[rootY] += 1
-
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        uf = UnionFind(n)
-
+        adjList = defaultdict(list)
         for u, v in edges:
-            uf.union(u, v)
-        
-        return len(set([uf.find(i) for i in range(n)]))
+            adjList[u].append(v)
+            adjList[v].append(u)
+
+        visited = set()
+
+        def dfs(node):
+            print(node)
+            if node in visited:
+                return
+            visited.add(node)
+
+            for nei in adjList[node]:
+                dfs(nei)
+            return
+
+        count = 0
+
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                count += 1
+        return count
