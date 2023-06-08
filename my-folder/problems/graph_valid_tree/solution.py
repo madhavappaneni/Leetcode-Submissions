ffd449b56459030c02e973a60c761531d116723d
Madhav Appaneni <appanenim@gmail.com>
2023-06-08 20:34:06 +0000

    Sync LeetCode submission - Graph Valid Tree (python3)

--- my-folder/problems/graph_valid_tree/solution.py
+++ my-folder/problems/graph_valid_tree/solution.py
@@ -1,67 +1,20 @@
-class UnionFind:
-    def __init__(self, size):
-        self.root = [None] * size
-        self.rank = [1] * size
-        for i in range(size):
-            self.root[i] = i
-        
-    def find(self, x):
-        if x == self.root[x]:
-            return x
-        self.root[x] = self.find(self.root[x])
-        return self.root[x]
-    
-    def connected(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        return rootX == rootY
-    
-    def union(self, x, y):
-        rootX = self.find(x)
-        rootY = self.find(y)
-        if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootY] > self.rank[rootX]:
-                self.root[rootX] = rootY
-            else:
-                self.root[rootX] = rootY
-                self.rank[rootY] += 1
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        uf = UnionFind(n)
-        count = 0
-        for u,v in edges:
-            if uf.connected(u, v):
+        adjList = defaultdict(list)
+        for u, v in edges:
+            adjList[u].append(v)
+            adjList[v].append(u)
+        
+        seen = set()
+        def dfs(node, parent):
+            if node in seen:
                 return False
-            count += 1
-            uf.union(u, v)
-        
-        return count == n - 1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
+            seen.add(node)
+            for nei in adjList[node]:
+                if nei == parent:
+                    continue
+                if not dfs(nei, node):
+                    return False
+            return True
+            
+        return dfs(0, -1) and n == len(seen)
