66c474c9eeeddb9724aac8af31c86eb8857bf118
Madhav Appaneni <appanenim@gmail.com>
2023-06-09 20:35:03 +0000

    Sync LeetCode submission - Course Schedule (python3)

--- my-folder/problems/course_schedule/solution.py
+++ my-folder/problems/course_schedule/solution.py
@@ -1,28 +1,27 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = [[] for _ in range(numCourses)]
-        for prereq in prerequisites:
-            graph[prereq[0]].append(prereq[1])
-        visited = [False] * numCourses
-        color = [False] * numCourses
-        
-        
-        def dfs(node):
-            if visited[node] == True:
-                return False
-            
-            if graph[node] == []:
-                return True
-            visited[node] = True
-            
-            for pre in graph[node]:
-                if not dfs(pre):
-                    return False
-            graph[node] = []
-            visited[node] = False
-            return True
+        adjList = defaultdict(list)
+        indeg = defaultdict(int)
+        queue = deque([])
+
+        for u, v in prerequisites:
+            adjList[u].append(v)
+            indeg[v] += 1
         
         for i in range(numCourses):
-            if not dfs(i):
-                return False
-        return True
+            if indeg[i] == 0:
+                queue.append(i)
+        
+        nodesVisited = 0
+
+        while queue:
+            p = queue.popleft()
+            nodesVisited += 1    
+            for nei in adjList[p]:
+                indeg[nei] -= 1
+                if indeg[nei] == 0:
+                    queue.append(nei)
+        
+        return nodesVisited == numCourses
+                
+
