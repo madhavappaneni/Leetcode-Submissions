db2ad4800396275dddfbdccf6ea773b1460168d0
Madhav Appaneni <appanenim@gmail.com>
2023-06-09 21:45:32 +0000

    Sync LeetCode submission - Parallel Courses (python3)

--- my-folder/problems/parallel_courses/solution.py
+++ my-folder/problems/parallel_courses/solution.py
@@ -1,40 +1,36 @@
 class Solution:
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
-        degree = [0] * (n + 1)
-        graph = collections.defaultdict(list)
+        adjList = defaultdict(list)
+        indeg = defaultdict(int)
+
+        for prevCourse, nextCourse in relations:
+            adjList[prevCourse].append(nextCourse)
+            indeg[nextCourse] += 1
         
-        for relation in relations:
-            graph[relation[0]].append(relation[1])
-            graph[relation[1]].append(relation[0])
-            degree[relation[1]] += 1
+        queue = deque([])
         
-        queue = collections.deque()
-        
-        for course, deg in enumerate(degree):
-            if deg == 0 and course != 0:
-                queue.append(course)
+        for i in range(1, n + 1):
+            if indeg[i] == 0:
+                queue.append(i)
+            
         if len(queue) == 0:
             return -1
         
-        # print(queue)
-        visited = set()
-        semestersCount = 0
+        numSemesters = 0
+        numCoursesCompleted = 0
+
         while queue:
-            semestersCount += 1
+            numSemesters += 1
             queueLen = len(queue)
+
             for i in range(queueLen):
-                elem = queue.popleft()
-                if elem in visited:
-                    continue
-                visited.add(elem)
-                for nei in graph[elem]:
-                    degree[nei] -= 1
-                    if degree[nei] == 0:
+                p = queue.popleft()
+                numCoursesCompleted += 1
+                for nei in adjList[p]:
+                    indeg[nei] -= 1
+                    if indeg[nei] == 0:
                         queue.append(nei)
-        # print(visited)
-        return semestersCount if len(visited) == n else -1
-                      
-                
         
-        
-            
+        return numSemesters if numCoursesCompleted == n else -1
+
+
