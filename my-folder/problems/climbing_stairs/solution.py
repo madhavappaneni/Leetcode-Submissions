4c2b7b4a28e30ab10002f8c80340ec9ae3d5a846
Madhav Appaneni <appanenim@gmail.com>
2023-06-13 21:26:13 +0000

    Sync LeetCode submission - Climbing Stairs (python3)

--- my-folder/problems/climbing_stairs/solution.py
+++ my-folder/problems/climbing_stairs/solution.py
@@ -1,10 +1,52 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        mem = [0, 1]
-        if n == 0 or n == 1 or n ==2:
-            return n
+        
+        
+        # def helper(n):
+        #     if n == 0 or n == 1 or n == 2:
+        #         return n
+        #     return helper(n - 1) + helper(n - 2)
+        
+        # return helper(n)
+            
+        # memo = [-1] * (n + 1)
 
+        # def helper(n):
+        #     if memo[n] != -1:
+        #         return memo[n]
+            
+        #     elif n in [0, 1, 2]:
+        #         return n
+            
+        #     memo[n] = helper(n - 1) + helper(n - 2)
+            
+        #     return memo[n]
+        
+        # return helper(n)
+
+
+        # memo = [-1] * (n + 1)
+
+        # if n <= 2:
+        #     return n
+
+        # memo[0], memo[1], memo[2] = 0, 1, 2
+
+
+        # for i in range(3, n + 1):
+        #     memo[i] = memo[i - 1] + memo[i - 2]
+        
+        # return memo[-1]
+
+
+
+
+        mem = [0, 1]
+
+        if n in [0, 1, 2]:
+            return n
+        
         for i in range(2, n + 1):
             mem[i % 2] = sum(mem)
+        
         return sum(mem)
-
