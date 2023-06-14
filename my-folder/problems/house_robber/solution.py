77a6b5be18f9218ee8b4ab80acca7f6df43dc59f
Madhav Appaneni <appanenim@gmail.com>
2023-06-14 19:04:10 +0000

    Sync LeetCode submission - House Robber (python3)

--- my-folder/problems/house_robber/solution.py
+++ my-folder/problems/house_robber/solution.py
@@ -1,25 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # @lru_cache()
-        # def helper(idx):
-        #     if idx > len(nums) - 1:
-        #         return 0
-        #     return max(nums[idx] + helper(idx + 2), helper(idx + 1))
-
-        # def helper(idx):
-        #     if idx < 0:
-        #         return 0
-        #     return max(nums[idx] + helper(idx - 2), helper(idx - 1))
-
-        # return helper(len(nums) - 1)
-
-        n = len(nums)
-        dp = [0] * n
-        if n <= 2:
-            return max(nums)
-
-        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
-        for i in range(2, n):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
         
-        return max(dp)
+        # @lru_cache(maxsize = None)
+        # def helper(i):
+        #     if i >= len(nums):
+        #         return 0
+            
+        #     return max(nums[i] + helper(i + 2), helper(i + 1))
+        
+        # return helper(0)
+    
+        # dp = [-1] * (len(nums))
+        # if len(nums) <= 2:
+        #     return max(nums)
+
+        # dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+
+        # for i in range(2, len(nums)):
+        #     dp[i] = max(dp[i-1] , dp[i-2] + nums[i])
+        
+        # return dp[-1]
+
+        robSecond = 0
+        robFirst = nums[0]
+
+        for i in range(1, len(nums)):
+            current = max(robFirst, robSecond + nums[i])
+            robSecond = robFirst
+            robFirst = current
+        
+        return robFirst
