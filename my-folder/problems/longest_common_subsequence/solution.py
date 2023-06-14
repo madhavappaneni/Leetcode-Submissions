99e7da9d2f072bfb7da9ca752da410d0ee8c0bb5
Madhav Appaneni <appanenim@gmail.com>
2023-06-14 21:08:14 +0000

    Sync LeetCode submission - Longest Common Subsequence (python3)

--- my-folder/problems/longest_common_subsequence/solution.py
+++ my-folder/problems/longest_common_subsequence/solution.py
@@ -11,15 +11,60 @@ class Solution:
         
         # return solve(len(text1) - 1, len(text2) - 1)
 
-        dp = [[0] * (len(text2) + 1) for _ in range(1 + len(text1))]
+        # dp = [[0] * (len(text2) + 1) for _ in range(1 + len(text1))]
         
-        for i in range(len(text1) + 1):
-            for j in range(len(text2) + 1):
+        # for i in range(len(text1) + 1):
+        #     for j in range(len(text2) + 1):
+        #         if i == 0 or j == 0:
+        #             dp[i][j] = 0
+        #         elif text1[i-1] == text2[j - 1]:
+        #             dp[i][j] = 1 + dp[i-1][j-1]
+        #         else:
+        #             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        
+        # return dp[-1][-1]
+
+        # @lru_cache(None)
+        # def solve(i, j):
+        #     if i < 0 or j < 0:
+        #         return 0
+        #     if text1[i] == text2[j]:
+        #         return 1 + solve(i - 1, j - 1)
+        #     else:
+        #         return max(solve(i - 1, j), solve(i, j - 1))
+        # return solve(len(text1) - 1, len(text2) - 1)
+
+        m, n = len(text1), len(text2)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+    
+        for i in range(m + 1):
+            for j in range(n + 1):
                 if i == 0 or j == 0:
                     dp[i][j] = 0
-                elif text1[i-1] == text2[j - 1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                elif text1[i-1] == text2[j-1]:
+                    dp[i][j] = dp[i-1][j-1] + 1
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
         
         return dp[-1][-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
