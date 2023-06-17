ded29452806ace0f696a293b6d5858b6508de66f
Madhav Appaneni <appanenim@gmail.com>
2023-06-17 21:26:04 +0000

    Sync LeetCode submission - Target Sum (python3)

--- my-folder/problems/target_sum/solution.py
+++ my-folder/problems/target_sum/solution.py
@@ -1,21 +1,13 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        count = [0]
-        targetSumMap = [[-1] * (len(nums) + 1) for _ in range(2 * sum(nums) + 1)]
-        def helper(idx, currTarget):
-            print(idx, currTarget, "test")
-            if idx == len(nums):
-                if currTarget == target:
-                    return 1
-                else:
-                    return 0
-            else:
-                if targetSumMap[currTarget + sum(nums)][idx] != -1:
-                    return targetSumMap[currTarget + sum(nums)][idx]
-                add = helper(idx + 1, currTarget + nums[idx])
-                sub = helper(idx + 1, currTarget - nums[idx])
-                targetSumMap[currTarget + sum(nums)][idx] = add + sub
-                return add + sub
-        return helper(0, 0)
+        
+        @lru_cache(maxsize = None)
+        def helper(currIdx, currTarget):
+            if currIdx == len(nums):
+                return int(currTarget == 0)
+
+            return helper(currIdx + 1, currTarget - nums[currIdx]) + helper(currIdx + 1, currTarget + nums[currIdx])
+            
+        return helper(0, target)
             
             
