5de89c47b47a0d47f0d74e5cffc4e3cd2fe65f0d
Madhav Appaneni <appanenim@gmail.com>
2023-06-23 21:17:18 +0000

    Sync LeetCode submission - Divide Array in Sets of K Consecutive Numbers (python3)

---
+++ my-folder/problems/divide_array_in_sets_of_k_consecutive_numbers/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPossibleDivide(self, nums: List[int], k: int) -> bool:
+
+        if len(nums) % k != 0:
+            return False
+            
+        count = Counter(nums)
+        minHeap = list(count.keys())
+        heapify(minHeap)
+
+        while minHeap:
+            first = minHeap[0]
+
+            for i in range(first, first + k):
+                if i not in count:
+                    return False
+                
+                count[i] -= 1
+
+                if count[i] == 0:
+                    heappop(minHeap)
+        return True
