6e4ee0ef2e3fc9e9d10ab89403e876ae94e1e6ee
Madhav Appaneni <appanenim@gmail.com>
2023-06-23 18:02:09 +0000

    Sync LeetCode submission - Merge Triplets to Form Target Triplet (python3)

--- my-folder/problems/merge_triplets_to_form_target_triplet/solution.py
+++ my-folder/problems/merge_triplets_to_form_target_triplet/solution.py
@@ -1,11 +1,18 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        out = [0] * 3
+
+        first, second, third = False, False, False
+
         for triplet in triplets:
             if triplet[0] > target[0] or triplet[1] > target[1] or triplet[2] > target[2]:
                 continue
-            for i, v in enumerate(triplet):
-                if v == target[i]:
-                    out[i] = max(out[i], v)
-        return out == target
-                
+            
+            first = first or (triplet[0] == target[0])
+            second = second or (triplet[1] == target[1])
+            third = third or (triplet[2] == target[2])
+
+            
+            if first and second and third:
+                return True
+        
+        return False
