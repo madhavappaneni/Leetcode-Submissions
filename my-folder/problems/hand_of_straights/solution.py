4d46ea9e98e4fc1db787e7455ec04a53bc25d042
Madhav Appaneni <appanenim@gmail.com>
2023-06-23 21:15:32 +0000

    Sync LeetCode submission - Hand of Straights (python3)

--- my-folder/problems/hand_of_straights/solution.py
+++ my-folder/problems/hand_of_straights/solution.py
@@ -1,12 +1,32 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        # if len(hand) % groupSize != 0:
+        #     return False
+            
+        # count = Counter(hand)
+        # minHeap = list(count.keys())
+        # heapify(minHeap)
+
+        # while minHeap:
+        #     first = minHeap[0]
+
+        #     for i in range(first, first + groupSize):
+        #         if i not in count:
+        #             return False
+                
+        #         count[i] -= 1
+
+        #         if count[i] == 0:
+        #             heappop(minHeap)
+        # return True
+
         hand.sort()
-        while len(hand) != 0:
-            temp = hand[0]
-            for i in range(temp, temp + groupSize):
-                if i in hand:
-                    hand.pop(hand.index(i))
-                else:
-                    # print(i, hand)
-                    return False
+
+        while hand:
+            try:
+                first = hand[0]
+                for i in range(groupSize):
+                    hand.remove(first + i)
+            except:
+                return False
         return True
