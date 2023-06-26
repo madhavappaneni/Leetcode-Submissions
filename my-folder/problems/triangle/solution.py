197c7da781cd49e37b9bdc3eb71b79979f3f8406
Madhav Appaneni <appanenim@gmail.com>
2023-06-26 19:46:10 +0000

    Sync LeetCode submission - Triangle (python3)

--- my-folder/problems/triangle/solution.py
+++ my-folder/problems/triangle/solution.py
@@ -1,11 +1,23 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-
-        @lru_cache(None)
-        def solve(level, idx):
-            if level == len(triangle) - 1:
-                return triangle[level][idx]
-            ans = triangle[level][idx] + min(solve(level + 1, idx), solve(level + 1, idx + 1))
-            return ans
         
-        return solve(0, 0)
+        # @lru_cache(None)
+        # def helper(level, idx):
+        #     if level == len(triangle):
+        #         return 0
+        #     return triangle[level][idx] + min(helper(level + 1, idx), helper(level + 1, idx + 1))
+        
+        # return helper(0, 0)
+
+        for i in range(1, len(triangle)):
+            prev = triangle[i - 1]
+            curr = triangle[i]
+
+            for j in range(len(curr)):
+                first, second = float('inf'), float('inf')
+                if 0 <= j - 1 < len(prev):
+                    first = prev[j - 1]
+                if 0 <= j < len(prev):
+                    second = prev[j]
+                curr[j] += min(first, second)
+        return min(triangle[-1])
