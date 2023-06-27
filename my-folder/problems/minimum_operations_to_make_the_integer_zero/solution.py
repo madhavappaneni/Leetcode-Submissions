b444ab6a9426f666061a6fbbd93282f64e2ed78c
Madhav Appaneni <appanenim@gmail.com>
2023-06-27 15:02:23 +0000

    Sync LeetCode submission - Minimum Operations to Make the Integer Zero (python3)

--- my-folder/problems/minimum_operations_to_make_the_integer_zero/solution.py
+++ my-folder/problems/minimum_operations_to_make_the_integer_zero/solution.py
@@ -1,19 +1,10 @@
 class Solution:
     def makeTheIntegerZero(self, num1: int, num2: int) -> int:
-        def getBitCount(n):
-            count = 0
-            while n > 0:
-                n &= n - 1
-                count += 1
-            return count
+        
+        for k in range(61):
+            target = num1 - k * num2
 
-        if num1 < num2:
-            return -1
-
-        for i in range(102):
-            diff = num1 - num2 * i
-            numBits = getBitCount(diff)
-            if numBits <= i <= diff:
-                return i
-
+            if target >= 0 and target.bit_count() <= k <= target:
+                return k
+        
         return -1
