40cee7ac4d8bfc3486a873009fb829bbc11e0ef5
Madhav Appaneni <appanenim@gmail.com>
2023-06-27 15:16:04 +0000

    Sync LeetCode submission - Ways to Split Array Into Good Subarrays (python3)

--- my-folder/problems/ways_to_split_array_into_good_subarrays/solution.py
+++ my-folder/problems/ways_to_split_array_into_good_subarrays/solution.py
@@ -1,21 +1,17 @@
 class Solution:
     def numberOfGoodSubarraySplits(self, nums: List[int]) -> int:
-        i = 0
         count = 0
-        zerosCount = 0
+        lo = 0
+        hi = 0
 
-        while i < len(nums) and nums[i] != 1:
-            i += 1
-
-        while i < len(nums):
-            if nums[i] == 0:
-                zerosCount += 1
-            elif nums[i] == 1:
+        while hi < len(nums):
+            if nums[hi] == 1:
                 if count == 0:
                     count = 1
-                if zerosCount > 0:
-                    count *= (zerosCount + 1) % (10 ** 9 + 7)
-                zerosCount = 0
-            i += 1
-        return count % (10 ** 9 + 7)
+                else:
+                    count *= (hi - lo)
+                    count %= (10 ** 9 + 7)
+                lo = hi
+            hi += 1
+        return count
 
