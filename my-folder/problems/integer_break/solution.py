390e6d8538cd51a6c606529bcd129c3628490388
Madhav Appaneni <appanenim@gmail.com>
2023-07-03 19:58:45 +0000

    Sync LeetCode submission - Integer Break (python3)

---
+++ my-folder/problems/integer_break/solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+
+
+        # @lru_cache(None)
+        # def helper(m):
+        #     if m == 1 or m == 2:
+        #         return 1
+        #     ans = 1
+        #     for i in range(1, m):
+        #         ans = max(ans, i * helper(m - i), i * (m - i))
+        #     return ans
+        
+        # return helper(n)
+
+        dp = [1] * (n + 1)
+        
+        for i in range(3, n + 1):
+            for j in range(1, i):
+                dp[i] = max(dp[i], (j) * dp[i - j], (j) * (i - j))
+                
+        return dp[-1]
