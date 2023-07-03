37ffe55c83f1d0ae6e0876735aa2d13f8bc6efc7
Madhav Appaneni <appanenim@gmail.com>
2023-07-03 20:58:53 +0000

    Sync LeetCode submission - Delete and Earn (python3)

--- my-folder/problems/delete_and_earn/solution.py
+++ my-folder/problems/delete_and_earn/solution.py
@@ -1,31 +1,20 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        # points = defaultdict(int)
-        # maxNum = float('-inf')
-        # for num in nums:
-        #     points[num] += num
-        #     maxNum = max(maxNum, num)
-        
-        # @lru_cache(None)
-        # def solve(num):
-        #     if num == 0:
-        #         return 0
-        #     if num == 1:
-        #         return points[num]
-        #     return max(solve(num - 1), solve(num - 2) + points[num])
-        
-        # return solve(maxNum)
-
         points = defaultdict(int)
-        maxNum = float('-inf')
+
         for num in nums:
             points[num] += num
-            maxNum = max(maxNum, num)
 
-        dp = [0] * (maxNum + 1)
-        dp[0] = 0
-        dp[1] = points[1]
-        for i in range(2, len(dp)):
-            dp[i] = max(dp[i-1], dp[i-2] + points[i])
+        maxNum = max(nums)
+
+        @lru_cache(None)
+        def helper(n):
+            if n == 0:
+                return 0
+            if n == 1:
+                return points[1]
+            
+            return max(helper(n - 1), helper(n - 2) + points[n])
         
-        return max(dp)
+        return helper(maxNum)
+        
