686801bc44d7acb1ba11af8eec3622cf26c3ee13
Madhav Appaneni <appanenim@gmail.com>
2023-07-07 21:47:22 +0000

    Sync LeetCode submission - Find Peak Element (python3)

--- my-folder/problems/find_peak_element/solution.py
+++ my-folder/problems/find_peak_element/solution.py
@@ -32,8 +32,10 @@ class Solution:
 
         while low < high:
             mid = (low + high) >> 1
+
             if nums[mid] < nums[mid + 1]:
                 low = mid + 1
             else:
                 high = mid
+        
         return low
