70e8cd7e348ca26fc3ce21d92216566ac2e92637
Madhav Appaneni <appanenim@gmail.com>
2023-07-09 23:24:15 +0000

    Sync LeetCode submission - Evaluate Reverse Polish Notation (python3)

--- my-folder/problems/evaluate_reverse_polish_notation/solution.py
+++ my-folder/problems/evaluate_reverse_polish_notation/solution.py
@@ -1,17 +1,15 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = deque()
-        operations = {
-            "+": lambda a, b: a + b,
-            "-": lambda a, b: a - b,
-            "*": lambda a, b: a * b,
-            "/": lambda a, b: a / b
-        }
+        stack = []
         for token in tokens:
-            if token not in ['+', '-', '*', '/']:
-                stack.append(token)
+            if token in ['+', '-', '*', '/']:
+                elem1, elem2 = int(stack.pop()), int(stack.pop())
+                ans = None
+                if token == '+': ans = elem1 + elem2
+                if token == '-': ans = elem2 - elem1
+                if token == '*': ans = elem1 * elem2
+                if token == '/': ans = elem2 / elem1
+                stack.append(ans)
             else:
-                b, a = int(stack.pop()), int(stack.pop())
-                operation = operations[token]
-                stack.append(operation(a, b))
+                stack.append(token)
         return int(stack[-1])
