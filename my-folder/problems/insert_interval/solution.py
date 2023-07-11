b344f7a81f82302592e1f6726d780d0041d84546
Madhav Appaneni <appanenim@gmail.com>
2023-07-11 19:00:20 +0000

    Sync LeetCode submission - Insert Interval (python3)

--- my-folder/problems/insert_interval/solution.py
+++ my-folder/problems/insert_interval/solution.py
@@ -1,18 +1,23 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        insertIndex = len(intervals)
-        for i, interval in enumerate(intervals):
-            # print(i, interval)
-            if interval[0] > newInterval[0]:
-                insertIndex = i
-                break
-        intervals.insert(insertIndex, newInterval)
-        # print(intervals)
-        
+        # out = []
+        # insertIdx = len(intervals)
+        # for i, currInterval in enumerate(intervals):
+        #     if currInterval[0] > newInterval[0]:
+        #         insertIdx = i
+        #         break
+        insertIdx = bisect_left(intervals, newInterval) 
+        intervals.insert(insertIdx, newInterval)
+
         out = []
+
         for interval in intervals:
-            if not out or out[-1][1] < interval[0]:
+            if not out or interval[0] > out[-1][1]:
                 out.append(interval)
             else:
                 out[-1][1] = max(out[-1][1], interval[1])
+        
         return out
+            
+            
+
