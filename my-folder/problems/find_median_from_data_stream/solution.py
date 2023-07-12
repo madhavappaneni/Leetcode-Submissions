ec2e35c286cad5719d82baa81d993088b7ecf7e0
Madhav Appaneni <appanenim@gmail.com>
2023-07-12 19:38:43 +0000

    Sync LeetCode submission - Find Median from Data Stream (python3)

--- my-folder/problems/find_median_from_data_stream/solution.py
+++ my-folder/problems/find_median_from_data_stream/solution.py
@@ -1,34 +1,48 @@
 class MedianFinder:
 
     def __init__(self):
-        self.small = [] #stores values < median
-        self.large = [] #stores values > mediam
-
+        self.large = []
+        self.small = []
+    
     def rebalanceHeaps(self):
-        # The following order is important
-        if self.large and self.small and -1 * self.small[0] > self.large[0]:
+        if self.large and self.small and self.large[0] < -1 * self.small[0]:
             heappush(self.large, -1 * heappop(self.small))
-
-        if len(self.large) - len(self.small) > 1:
-            heappush(self.small, -1 * heappop(self.large))
-        
         if len(self.small) - len(self.large) > 1:
             heappush(self.large, -1 * heappop(self.small))
-
-
+        if len(self.large) - len(self.small) > 1:
+            heappush(self.small, -1 * heappop(self.large))
 
     def addNum(self, num: int) -> None:
-        heapq.heappush(self.small, -1 * num)
+        heappush(self.small, -1 * num)
         self.rebalanceHeaps()
-        
 
     def findMedian(self) -> float:
         if len(self.large) == len(self.small):
-            return (self.large[0] - self.small[0]) / 2
+            return (self.large[0] - self.small[0])/2
         elif len(self.large) > len(self.small):
             return self.large[0]
         else:
-            return -1 * self.small[0]
+            return -self.small[0]
+        
+
+# class MedianFinder:
+
+#     def __init__(self):
+#         self.data = []
+#         self.dataLen = 0
+
+#     def addNum(self, num: int) -> None:
+#         insertIdx = bisect_left(self.data, num)
+#         self.data.insert(insertIdx, num)
+#         self.dataLen += 1
+
+
+#     def findMedian(self) -> float:
+#         if self.dataLen % 2 == 0:
+#             return (self.data[self.dataLen // 2] + self.data[self.dataLen // 2 - 1]) / 2
+#         else:
+#             return self.data[self.dataLen // 2]
+        
 
 
 # Your MedianFinder object will be instantiated and called as such:
