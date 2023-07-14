79656aa92f22d76f5807b6d88ea063861f2ba87f
Madhav Appaneni <appanenim@gmail.com>
2023-07-14 18:46:49 +0000

    Sync LeetCode submission - Spiral Matrix (python3)

--- my-folder/problems/spiral_matrix/solution.py
+++ my-folder/problems/spiral_matrix/solution.py
@@ -2,11 +2,10 @@ class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         if not matrix:
             return matrix
-        left, right, top, bottom = 0, len(matrix[0]) - 1, 0, len(matrix) - 1
         ans = []
+        left, right, top, bottom = 0, len(matrix[0]) - 1, 0, len(matrix) - 1
         direction = 0
-
-        while len(ans) < len(matrix) * len(matrix[0]):
+        while len(ans) < len(matrix[0]) * len(matrix):
             if direction == 0:
                 for i in range(left, right + 1):
                     ans.append(matrix[top][i])
@@ -20,9 +19,10 @@ class Solution:
                     ans.append(matrix[bottom][i])
                 bottom -= 1
             if direction == 3:
-                for i in range(bottom, top - 1, -1):
+                for i in range(bottom, top - 1, - 1):
                     ans.append(matrix[i][left])
                 left += 1
-
             direction = (direction + 1) % 4
-        return (ans)
+        
+        return ans
+            
