8960f7f79dd7f14dfffb968123c3a18285de5e97
Madhav Appaneni <appanenim@gmail.com>
2023-07-18 21:49:20 +0000

    Sync LeetCode submission - Integer Break (python3)

--- my-folder/problems/integer_break/solution.py
+++ my-folder/problems/integer_break/solution.py
@@ -2,21 +2,32 @@ class Solution:
     def integerBreak(self, n: int) -> int:
 
 
-        # @lru_cache(None)
-        # def helper(m):
-        #     if m == 1 or m == 2:
-        #         return 1
-        #     ans = 1
-        #     for i in range(1, m):
-        #         ans = max(ans, i * helper(m - i), i * (m - i))
-        #     return ans
+        # # @lru_cache(None)
+        # # def helper(m):
+        # #     if m == 1 or m == 2:
+        # #         return 1
+        # #     ans = 1
+        # #     for i in range(1, m):
+        # #         ans = max(ans, i * helper(m - i), i * (m - i))
+        # #     return ans
         
-        # return helper(n)
+        # # return helper(n)
 
-        dp = [1] * (n + 1)
+        # dp = [1] * (n + 1)
         
-        for i in range(3, n + 1):
-            for j in range(1, i):
-                dp[i] = max(dp[i], (j) * dp[i - j], (j) * (i - j))
+        # for i in range(3, n + 1):
+        #     for j in range(1, i):
+        #         dp[i] = max(dp[i], (j) * dp[i - j], (j) * (i - j))
                 
-        return dp[-1]
+        # return dp[-1]
+
+        @lru_cache(None)
+        def helper(m):
+            if m == 1 or m == 2:
+                return 1
+            ans = 1
+            for i in range(1, m):
+                ans = max(ans, i * helper(m - i), i * (m - i))
+            return ans
+        
+        return helper(n)
