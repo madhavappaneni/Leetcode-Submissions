d139bdd14833ba65b9492f9b62267d0193a2d94d
Madhav Appaneni <appanenim@gmail.com>
2023-07-19 18:53:56 +0000

    Sync LeetCode submission - Minimum Cost For Tickets (python3)

--- my-folder/problems/minimum_cost_for_tickets/solution.py
+++ my-folder/problems/minimum_cost_for_tickets/solution.py
@@ -1,34 +1,26 @@
 class Solution:
-    def mincostTickets(self, days, costs) -> int:
-
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        
+        # @lru_cache(None)
         # def helper(idx):
-        #     print(idx)
         #     if idx >= len(days):
         #         return 0
-        #     return min(
-
-        #     )
+            
+        #     first = costs[0] + helper(bisect_left(days, days[idx] + 1))
+        #     second = costs[1] + helper(bisect_left(days, days[idx] + 7))
+        #     third = costs[2] + helper(bisect_left(days, days[idx] + 30))
+            
+        #     return min(first, second, third)
+        
         # return helper(0)
 
-        # dp = [0] * (len(days) + 1)
-    
-        # for i, x in enumerate(days):
-        #     dp[i + 1] = min(
-        #         dp[bisect.bisect(days, x - d, 0, i)] + c
-        #         for d, c in zip((1, 7, 30), costs)
-        #     )
+        dp = [0] * (len(days) + 1)
+
+        for idx in range(len(days) - 1, -1, -1):
+            dp[idx] = min(
+                costs[0] + dp[bisect_left(days, days[idx] + 1)],
+                costs[1] + dp[bisect_left(days, days[idx] + 7)],
+                costs[2] + dp[bisect_left(days, days[idx] + 30)]
+            )
         
-        # return dp[-1]
-
-        dayset = set(days)
-        durations = [1, 7, 30]
-
-        @lru_cache(None)
-        def dp(i):
-            if i > 365:
-                return 0
-            elif i in dayset:
-                return min(dp(i + d) + c for c, d in zip(costs, durations))
-            else:
-                return dp(i + 1)
-        return dp(1)
+        return dp[0]
